9217608b2c8ee06b2ec4c4f80ac35b8db4d95ea6
Ironplay3 <igorcherdakli049@gmail.com>
2023-04-24 13:01:17 +0200

    2

--- Assets/NewScript.cs
+++ Assets/NewScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class NewScript : MonoBehaviour
 {
+    public int _num = 2;
+    public string word;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world" + _num);
+
     }
 
     // Update is called once per frame
